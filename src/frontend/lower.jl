d5d7cdeb1e8d11b3731b9501b94348680ac43dc2
Mike J Innes <mike.j.innes@gmail.com>
2019-07-19 10:07:36 +0100

    if expressions

--- src/frontend/lower.jl
+++ src/frontend/lower.jl
@@ -1,45 +1,67 @@
 using IRTools
-using IRTools: IR, Slot, argument!
+using IRTools: IR, Slot, argument!, blocks
+
+# don't continue lowering after return
+# e.g. `f(return 1)`
+_push!(ir::IR, x) = IRTools.canbranch(blocks(ir)[end]) && push!(ir, x)
+
+# lower while ignoring return value (if applicable)
+_lower!(ir, x) = lower!(ir, x)
 
 isfn(x) = x isa Block && x.name == :fn
 
 lower!(ir::IR, x::Integer) = x
 lower!(ir::IR, x::Symbol) = Slot(x)
-lower!(ir::IR, x::Vector) = foreach(x -> lower!(ir, x), x)
+lower!(ir::IR, x::Vector) = (foreach(x -> _lower!(ir, x), x[1:end-1]); lower!(ir, x[end]))
 
 function lower!(ir::IR, ex::Operator)
   if ex.op == :(:=)
-    push!(ir, :($(Slot(ex.args[1])) = $(lower!(ir, ex.args[2]))))
+    x = Slot(ex.args[1])
+    _push!(ir, :($(x) = $(lower!(ir, ex.args[2]))))
+    return x
   else
-    push!(ir, Base.Expr(:call, ex.op, map(x -> lower!(ir, x), ex.args)...))
+    _push!(ir, Base.Expr(:call, ex.op, map(x -> lower!(ir, x), ex.args)...))
   end
 end
 
 function lower!(ir::IR, ex::Return)
   IRTools.return!(ir, lower!(ir, ex.val))
+  return
 end
 
-function lower!(ir::IR, ex::If)
-  b = IRTools.blocks(ir)[end]
+function lower!(ir::IR, ex::If, value = true)
+  b = blocks(ir)[end]
   ts = []
+  vs = []
+  body!(ir, ex) =
+    value ?
+      push!(vs, lower!(ir, ex)) :
+      _lower!(ir, ex)
   for (cond, body) in zip(ex.cond, ex.body)
     if cond === true
-      lower!(ir, body)
+      body!(ir, body)
+      push!(ts, b)
       b = IRTools.block!(ir)
       break
     end
     cond = lower!(ir, cond)
     t = IRTools.block!(ir)
     push!(ts, t)
-    lower!(ir, body)
+    body!(ir, body)
     f = IRTools.block!(ir)
     IRTools.branch!(b, f, unless = cond)
     b = f
   end
-  for t in ts
-    IRTools.canbranch(t) && IRTools.branch!(t, b)
+  for i = 1:length(ts)
+    IRTools.canbranch(ts[i]) &&
+      value ?
+        IRTools.branch!(ts[i], b, vs[i]) :
+        IRTools.branch!(ts[i], b)
   end
+  value && IRTools.argument!(b, insert = false)
 end
+
+_lower!(ir::IR, ex::If) = lower!(ir, ex, false)
 
 function lower!(ir::IR, ex::Block)
   if ex.name == :while
@@ -60,9 +82,8 @@ function lowerfn(ex)
   for arg in ex.args[1].args
     push!(ir, :($(Slot(arg)) = $(argument!(ir))))
   end
-  for x in ex.block
-    lower!(ir, x)
-  end
+  out = lower!(ir, ex.block)
+  out == nothing || IRTools.return!(ir, out)
   return ir |> IRTools.ssa! |> IRTools.prune!
 end
 
