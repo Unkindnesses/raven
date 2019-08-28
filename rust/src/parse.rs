1f69e7c1fac9ff2817b9d4497535a43ab254f3ae
Mike J Innes <mike.j.innes@gmail.com>
2019-08-28 10:21:01 +0100

    number parsing

--- rust/src/parse.rs
+++ rust/src/parse.rs
@@ -1,12 +1,52 @@
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufReader,SeekFrom};
 use std::io::prelude::*;
 
 #[derive(Debug)]
 pub enum Expr {
     Float(f64),
     Symbol(String),
-    Call(Vec<Expr>),
+    List(Vec<Expr>),
+}
+
+use Expr::*;
+
+// TODO: unicode
+fn read<T: Read>(buf: &mut T) -> char {
+    let mut byte = 0;
+    buf.read(std::slice::from_mut(&mut byte)).unwrap();
+    let char = char::from(byte);
+    return char;
+}
+
+fn peekf<T: Read + Seek,O>(buf: &mut T, f: &dyn Fn(&mut T)->O) -> O {
+    let pos = buf.seek(SeekFrom::Current(0)).unwrap();
+    let result = f(buf);
+    buf.seek(SeekFrom::Start(pos)).unwrap();
+    return result;
+}
+
+fn peek<T: Read + Seek>(buf: &mut T) -> char {
+    peekf(buf, &read)
+}
+
+fn number<T: Read + Seek>(buf: &mut T) -> Option<Expr> {
+    let mut s = String::new();
+    loop {
+        let ch = read(buf);
+        if ('0'..='9').contains(&ch) {
+            s.push(ch);
+        } else {
+            if ch != '\u{0}' { buf.seek(SeekFrom::Current(-1)).unwrap(); };
+            break;
+        }
+    }
+    if s.is_empty() { return None; }
+    return Some(Float(s.parse::<f64>().unwrap()));
+}
+
+fn parse<T: Read + Seek>(buf: &mut T) -> Option<Expr> {
+    return number(buf);
 }
 
 fn loadfile(path: &str) -> String {
@@ -18,10 +58,11 @@ fn loadfile(path: &str) -> String {
 }
 
 pub fn parsefile(path: &str) -> Expr {
-    let code = loadfile(path);
-    println!("Code: {}", code);
-    Expr::Call(
-        vec![Expr::Symbol(String::from("+")),
-             Expr::Float(5.5),
-             Expr::Float(1.1)])
+    let file = File::open(path).unwrap();
+    let mut buf = BufReader::new(file);
+    let code = parse(&mut buf);
+    return code.unwrap();
+    // List(vec![Symbol(String::from("+")),
+    //           Float(5.5),
+    //           Float(1.1)])
 }
