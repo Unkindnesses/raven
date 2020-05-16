09538990ca58b3b88af6923440ff517cdd6871e3
Mike J Innes <mike.j.innes@gmail.com>
2020-05-16 13:41:11 +0100

    space override

--- editor/raven-language/src/extension.ts
+++ editor/raven-language/src/extension.ts
@@ -1,8 +1,20 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('raven.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello from Raven!');
+	let disposable = vscode.commands.registerTextEditorCommand('raven.space', (editor, edit) => {
+		if (editor.selections.length == 1 && editor.selection.isEmpty) {
+			edit.insert(editor.selection.active, ' ');
+			return;
+		}
+		// Fallback: simulate standard behaviour
+		editor.selections.forEach((selection, i) => {
+			if (selection.isEmpty) {
+				edit.insert(selection.active, ' ');
+			} else {
+				edit.replace(selection, ' ');
+				editor.selections[i] = new vscode.Selection(selection.end, selection.end);
+			}
+		});
 	});
 	context.subscriptions.push(disposable);
 }
