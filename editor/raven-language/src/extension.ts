dd4f29ab9988465afd25b028033208711ad0ae3e
Mike J Innes <mike.j.innes@gmail.com>
2020-05-17 12:12:05 +0100

    implement space handling

--- editor/raven-language/src/extension.ts
+++ editor/raven-language/src/extension.ts
@@ -1,10 +1,30 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerTextEditorCommand('raven.space', (editor, edit) => {
+	let disposable = vscode.commands.registerTextEditorCommand('raven.space', async (editor, edit) => {
 		if (editor.selections.length == 1 && editor.selection.isEmpty) {
-			edit.insert(editor.selection.active, ' ');
-			return;
+			let position = editor.selection.active;
+			// `{|}` -> `{ | }`
+			let start = new vscode.Position(position.line, position.character-1);
+			var end   = new vscode.Position(position.line, position.character+1);
+			var text = editor.document.getText(new vscode.Range(start, end));
+			if (text == '{}') {
+				await editor.edit(edit => { // Force this to happen before the cursor change.
+					edit.insert(editor.selection.active, '  ');
+				});
+				let pos = new vscode.Position(position.line, position.character+1);
+				editor.selection = new vscode.Selection(pos, pos);
+				return;
+			}
+			// `| }` -> ` |}`
+			var end   = new vscode.Position(position.line, position.character+2);
+			var text = editor.document.getText(new vscode.Range(position, end));
+			console.log(text);
+			if (text == ' }') {
+				let pos = new vscode.Position(position.line, position.character+1);
+				editor.selection = new vscode.Selection(pos, pos);
+				return;
+			}
 		}
 		// Fallback: simulate standard behaviour
 		editor.selections.forEach((selection, i) => {
