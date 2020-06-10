6838341f62c58be70700d0d776e25ab7d27fd968
Mike J Innes <mike.j.innes@gmail.com>
2020-06-10 20:07:02 +0100

    pipe operator

--- src/frontend/parse.jl
+++ src/frontend/parse.jl
@@ -112,7 +112,7 @@ function symbol(io)
 end
 
 operators = ["=", "==", "+", "-", "*", "/", "^", ">", "<", ">=", "<=", ":",
-             ".", "&", "|"]
+             ".", "&", "|", "|>"]
 opchars = unique(reduce(*, operators))
 
 function op_token(io::IO)
