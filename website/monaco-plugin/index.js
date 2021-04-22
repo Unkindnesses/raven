ae1729b724d177153b60f49706502fb8e80f3def
Mike Innes <mike.j.innes@gmail.com>
2021-04-22 19:00:26 +0100

    update configurewebpack style

--- website/monaco-plugin/index.js
+++ website/monaco-plugin/index.js
@@ -4,11 +4,10 @@ module.exports = function(context, options) {
     return {
         name: 'monaco-plugin',
         configureWebpack(config, isServer, utils) {
-            config.module.rules.push(
-                {test: /\.ttf$/, use: ['file-loader']}
-            );
-            config.plugins.push(new MonacoWebpackPlugin());
-            return {};
+            return {
+                module: {rules: [{test: /\.ttf$/, use: ['file-loader']}]},
+                plugins: [new MonacoWebpackPlugin()]
+            };
         }
     }
 }
