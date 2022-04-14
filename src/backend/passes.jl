8c17d953a1c02606c2b349352b7073e0365fb42f
Mike Innes <mike.j.innes@gmail.com>
2022-04-14 12:06:24 +0100

    remove unreachable branches

--- src/backend/passes.jl
+++ src/backend/passes.jl
@@ -1,15 +1,28 @@
 blockargtype(mod::RModule, bl, i) = exprtype(mod, bl.ir, arguments(bl)[i])
 
+function isreachable(bl)
+  for (v, st) in bl
+    st.type == ⊥ && return false
+  end
+  return true
+end
+
 function casts!(mod::RModule, ir)
-  for bl in blocks(ir), br in branches(bl)
-    isreturn(br) && continue # TODO: handle multiple returns
-    for i = 1:length(arguments(br))
-      S = exprtype(mod, ir, arguments(br)[i])
-      T = blockargtype(mod, block(ir, br.block), i)
-      S == T && continue
-      arguments(br)[i] =
-        push!(bl, IRTools.stmt(Base.Expr(:call, :cast, T, arguments(br)[i]),
-                               type = T))
+  for bl in blocks(ir)
+    if !isreachable(bl)
+      empty!(branches(bl))
+      continue
+    end
+    for br in branches(bl)
+      isreturn(br) && continue # TODO: handle multiple returns
+      for i = 1:length(arguments(br))
+        S = exprtype(mod, ir, arguments(br)[i])
+        T = blockargtype(mod, block(ir, br.block), i)
+        S == T && continue
+        arguments(br)[i] =
+          push!(bl, IRTools.stmt(Base.Expr(:call, :cast, T, arguments(br)[i]),
+                                 type = T))
+      end
     end
   end
   return ir
