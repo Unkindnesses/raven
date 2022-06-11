5dd1073c18404597190f53757f254f4987f50d3f
Mike Innes <mike.j.innes@gmail.com>
2022-06-11 02:06:21 +0100

    simplify indexes into known tuples

--- src/middle/utils.jl
+++ src/middle/utils.jl
@@ -6,11 +6,16 @@ end
 function tuplecse(ir)
   pr = IRTools.Pipe(ir)
   env = Dict{Any,Variable}()
+  tuples = Dict{Variable,Expr}()
   for (v, st) in pr
     if isexpr(st.expr, :ref, :tuple)
+      isexpr(st.expr, :tuple) && (tuples[v] = st.expr)
       ex = IRTools.substitute(pr, st.expr)
       key = (ex, st.type)
-      if haskey(env, key)
+      if isexpr(st.expr, :ref) && st.type == Any && haskey(tuples, st.expr.args[1])
+        delete!(pr, v)
+        replace!(pr, v, tuples[st.expr.args[1]].args[st.expr.args[2]])
+      elseif haskey(env, key)
         delete!(pr, v)
         replace!(pr, v, env[key])
       else
