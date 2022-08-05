81e0e34ee382ff8b2233a6bfffa7b363d60c2471
Mike Innes <mike.j.innes@gmail.com>
2022-08-05 23:31:03 +0100

    concat fix

--- test/compiler.jl
+++ test/compiler.jl
@@ -21,7 +21,7 @@ fails(test) = result_code(test) == 1
 
 @testset for test in [:pow, :ptr, :relu, :complex, :structures, :swap,
                       :splat, :scope, :malloc0, :malloc1, :malloc2,
-                      :patterns]
+                      :patterns, :string]
   for r in result(test)
     @test r
   end
