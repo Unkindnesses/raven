5bb9ea6bdc97347830091653b82f7894646af164
Mike Innes <mike.j.innes@gmail.com>
2022-08-06 00:23:04 +0100

    don't bother with tuplecse

--- src/middle/utils.jl
+++ src/middle/utils.jl
@@ -4,30 +4,6 @@ function Base.replace!(pr::IRTools.Pipe, x, y)
   IRTools.substitute!(pr, x, IRTools.substitute(pr, y))
 end
 
-# TODO not right if we assume tuples concatenate; need to check types
-function tuplecse(ir)
-  pr = IRTools.Pipe(ir)
-  env = Dict{Any,Variable}()
-  tuples = Dict{Variable,Expr}()
-  for (v, st) in pr
-    if isexpr(st.expr, :ref, :tuple)
-      isexpr(st.expr, :tuple) && (tuples[v] = st.expr)
-      ex = IRTools.substitute(pr, st.expr)
-      key = (ex, st.type)
-      if isexpr(st.expr, :ref) && st.type == Any && haskey(tuples, st.expr.args[1])
-        delete!(pr, v)
-        replace!(pr, v, tuples[st.expr.args[1]].args[st.expr.args[2]])
-      elseif haskey(env, key)
-        delete!(pr, v)
-        replace!(pr, v, env[key])
-      else
-        env[key] = v
-      end
-    end
-  end
-  return IRTools.finish(pr)
-end
-
 reachable(b::IRTools.Block) =
   any(((v, st),) -> st.type == ⊥, b) ? Set(b.id) :
     reduce(Base.union, [Set(b.id),
