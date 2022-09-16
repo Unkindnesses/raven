a7c8fe306342d7dab275993e7cc798ecb1da359d
Mike Innes <mike.j.innes@gmail.com>
2022-09-16 10:05:54 +0100

    organise refcount.jl

--- src/middle/refcount.jl
+++ src/middle/refcount.jl
@@ -67,22 +67,25 @@ isreftype(x::Recursive) = true
 
 isglobal(ir, v) = haskey(ir, v) && isexpr(ir[v].expr, :global)
 
-function retain(f, T::VPack, x)
-  ptr = f(stmt(Expr(:ref, x, 2), type = rlist(pack(:Ptr, Int32))))
-  f(stmt(xcall(:retain!, ptr), type = pack(:Nil)))
-end
-
-function retain(f, T::Recursive, x)
-  ptr = f(stmt(Expr(:ref, x, 1), type = rlist(pack(:Ptr, Int32))))
-  f(stmt(xcall(:retain!, ptr), type = pack(:Nil)))
-end
-
 function retain(f, T::Pack, x)
   for i = 0:nparts(T)
     isreftype(part(T, i)) || continue
     p = _indexer!(f, T, i, x)
     retain(f, part(T, i), p)
   end
+end
+
+function release(f, T::Pack, x)
+  for i = 0:nparts(T)
+    isreftype(part(T, i)) || continue
+    p = _indexer!(f, T, i, x)
+    release(f, part(T, i), p)
+  end
+end
+
+function retain(f, T::VPack, x)
+  ptr = f(stmt(Expr(:ref, x, 2), type = rlist(pack(:Ptr, Int32))))
+  f(stmt(xcall(:retain!, ptr), type = pack(:Nil)))
 end
 
 function release(f, T::VPack, x)
@@ -98,17 +101,14 @@ function release(f, T::Or, x)
   # TODO implement
 end
 
+function retain(f, T::Recursive, x)
+  ptr = f(stmt(Expr(:ref, x, 1), type = rlist(pack(:Ptr, Int32))))
+  f(stmt(xcall(:retain!, ptr), type = pack(:Nil)))
+end
+
 function release(f, T::Recursive, x)
   ptr = f(stmt(Expr(:ref, x, 1), type = rlist(pack(:Ptr, Int32))))
   f(stmt(xcall(:release!, ptr), type = pack(:Nil)))
-end
-
-function release(f, T::Pack, x)
-  for i = 0:nparts(T)
-    isreftype(part(T, i)) || continue
-    p = _indexer!(f, T, i, x)
-    release(f, part(T, i), p)
-  end
 end
 
 function refcounts!(ir)
